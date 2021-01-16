058d50f33c939f753c207a134ef2a490b66d1e0c
Yu Yun Liu <yu.yun.liu@mail.mcgill.ca>
2021-01-16 14:45:32 -0500

    setup fb sdk

--- front/src/Login.js
+++ front/src/Login.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export const Login = () => {
     return (
-        <h1> Hello </h1>
-)
+        // <h1> Hello </h1>
+        <div
+            class="fb-like"
+            data-share="true"
+            data-width="450"
+            data-show-faces="true">
+        </div>
+    )
 }
